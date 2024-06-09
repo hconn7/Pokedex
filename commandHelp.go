3398a17171ecc7ea6c75822d215c7d8b902d142c
Henry Conner <henryconner10@gmail.com>
2024-06-08 22:58:18 -0700

    AHAgit add .

--- commandHelp.go
+++ commandHelp.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func commandHelp() error {
 	fmt.Println("Oh dear! Lets get you some help...")
-	for _, cmd := range getComands() {
+	for _, cmd := range getCommands() {
 		fmt.Println("available commands: %s, %s\n", cmd.name, cmd.description)
 	}
 	fmt.Println()
